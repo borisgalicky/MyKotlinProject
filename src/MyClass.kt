4342cb95d0c51bf38cb8da61a69c15e116406429
Boris Galicky <boris.galicky@akademiasovy.sk>
2018-10-15 08:22:44 +0200

    init repo

---
+++ src/MyClass.kt
@@ -0,0 +1,58 @@
+import java.util.*
+
+fun main(args: Array<String>){
+    /*println("Enter your name:");
+    var name = readLine();
+    println("Enter your age");
+    var age: Int = Integer.valueOf(readLine());
+    println("Hi! Your name is $name and you're $age years old!");
+
+    var a: Int = 20;
+    var b: Int = 10;
+    println("$a + $b = ${a+b}");
+    println("$a - $b = ${a-b}");
+    println("$a * $b = ${a*b}");
+    println("$a : $b = ${a/b}");
+
+    var numbers = arrayOf<Int>();
+    for(i in 0 until 10){ //for(var i=0;i<10;i++)
+        numbers+=i+1;
+        numbers[i]*=3;
+        println(numbers[i]);
+    }
+    val scan = Scanner(System.`in`);
+    val num = scan.nextLine().trim().toInt();
+    when(num){
+        1 -> println("Monday");
+        2 -> println("Tuesday");
+        3 -> println("Wednesday");
+        4 -> println("Thursday");
+        5 -> println("Friday");
+        6 -> println("Saturday");
+        7 -> println("Sunday");
+        else -> println("Not a day");
+    }
+    val cities = arrayOf("Bratislava","Košice","Prešov","Žilina","Banská Bystrica","Nitra","Trnava","Trenčín","Martin","Poprad");
+    for(i in cities){
+        println(i);
+    }
+    for(i in 0..10){
+        println("${(3*i)+2}");
+    }
+    for(i in 1..15){
+        println(i);
+    }
+    for(i in 50 downTo 1 step 2) {
+        println(i);
+    }
+    val set: MutableSet<Int> = mutableSetOf(2,4,10,5,8,21,6,0,15,18);
+    loop@for(i in set){
+        println(i);
+        if(i==0){
+            break@loop;
+        }
+        else{
+            continue
+        }
+    }*/
+}
